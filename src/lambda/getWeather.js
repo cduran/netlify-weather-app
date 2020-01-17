330b24789c59c88244e9da27536061497bbb3c28
Carlos Duran <c.duran.1983@gmail.com>
2020-01-17 04:29:25 -0600

    Removing proxy from package.json Fixing weather API url

--- src/lambda/getWeather.js
+++ src/lambda/getWeather.js
@@ -5,7 +5,7 @@ const API_KEY = process.env.API_KEY;
 
 exports.handler = async (event, context) => {
     const city = JSON.parse(event.body).city;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${API_KEY}`;
 
     console.log(API_KEY);
     return fetch(url, {headers: { "Accept": "application/json" } })
